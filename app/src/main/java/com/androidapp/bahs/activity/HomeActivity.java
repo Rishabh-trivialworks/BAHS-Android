88f3c3173941a71cf6078337c1c68592bf8588dc
rishabh <rishabh@mobikasa.com>
2016-12-13 06:00:53 +0530

    generate devce token through FireBase

--- app/src/main/java/com/androidapp/bahs/activity/HomeActivity.java
+++ app/src/main/java/com/androidapp/bahs/activity/HomeActivity.java
@@ -3,6 +3,7 @@ import android.os.Bundle;
 
 import com.androidapp.bahs.R;
 import com.androidapp.bahs.activity.base.BaseActivity;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class HomeActivity extends BaseActivity {
 
@@ -10,5 +11,11 @@ public class HomeActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+       initializeFirebaseToken();
+    }
+
+    private void initializeFirebaseToken() {
+        String token = FirebaseInstanceId.getInstance().getToken();
+        System.out.print("FCM Registratio: " + token);
     }
 }
